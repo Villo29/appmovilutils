b134dcd859fc27eecf1eb6af874eafc8507b1cdf
David Ruiz <jesuruga@gmail.com>
2024-10-21 09:38:36 -0600

    Add empty files for text, speech, qrflutter, and sensorplus pages

---
+++ android/app/src/main/kotlin/com/appmovil/villo/appmovilutils/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.appmovil.villo.appmovilutils
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:appmovilutils/pages/home.dart';
+import 'package:appmovilutils/pages/geolocator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Phone App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+        ),
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  static final List<Widget> _pages = <Widget>[
+    const Home(title: 'Home Page'),
+    const LocationStatusScreen(),
+    const RequestPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Geolacalización',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.http),
+            label: 'QR',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.phone),
+            label: 'Sensores',),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.mic),
+              label: 'Habla',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.audiotrack),
+              label: 'Sonido',
+            ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Color.fromARGB(255, 232, 27, 27),
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class RequestPage extends StatelessWidget {
+  const RequestPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Request Page'),
+      ),
+      body: const Center(
+        child: Text('This is the Request Page'),
+      ),
+    );
+  }
+}
